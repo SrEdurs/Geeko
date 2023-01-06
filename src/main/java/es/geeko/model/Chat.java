ace6da9a3b7e36a0d2bafdccdd3ba1eed7b5cc83
Eduardo Rojas <edu_mairenero@hotmail.com>
2023-01-06 12:54:11 +0100

    - Creada la clase Chat.java

--- src/main/java/es/geeko/model/Chat.java
+++ src/main/java/es/geeko/model/Chat.java
@@ -8,6 +8,7 @@ import java.util.List;
 @Entity
 @Table(name = "chats")
 public class Chat {
+    dhjsbjshdbcjhsdbcjhsdbjchbsdhjc
     @Id
     @Column(name="id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
