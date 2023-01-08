1f0d480d7434477d00ec600303b9faa36a58e39f
Eduardo Rojas <edu_mairenero@hotmail.com>
2023-01-08 18:55:12 +0100

    - Getters y Setters de Chat

--- src/main/java/es/geeko/model/Chat.java
+++ src/main/java/es/geeko/model/Chat.java
@@ -42,4 +42,100 @@ public class Chat {
     @OneToMany(mappedBy = "mensajes")
     List<Mensaje> mensajes;
 
+    public Chat() {
+    }
+
+    public Chat(int id, int idDestinatario, int idRemitente, String titulo) {
+        this.id = id;
+        this.idDestinatario = idDestinatario;
+        this.idRemitente = idRemitente;
+        this.titulo = titulo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getIdDestinatario() {
+        return idDestinatario;
+    }
+
+    public void setIdDestinatario(int idDestinatario) {
+        this.idDestinatario = idDestinatario;
+    }
+
+    public int getIdRemitente() {
+        return idRemitente;
+    }
+
+    public void setIdRemitente(int idRemitente) {
+        this.idRemitente = idRemitente;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public int getActivo() {
+        return activo;
+    }
+
+    public void setActivo(int activo) {
+        this.activo = activo;
+    }
+
+    public List<Usuario> getUsuarios() {
+        return usuarios;
+    }
+
+    public void setUsuarios(List<Usuario> usuarios) {
+        this.usuarios = usuarios;
+    }
+
+    public List<Mensaje> getMensajes() {
+        return mensajes;
+    }
+
+    public void setMensajes(List<Mensaje> mensajes) {
+        this.mensajes = mensajes;
+    }
+
+    @Override
+    public String toString() {
+        return "Chat{" +
+                "id=" + id +
+                ", idDestinatario=" + idDestinatario +
+                ", idRemitente=" + idRemitente +
+                ", titulo='" + titulo + '\'' +
+                ", imagen='" + imagen + '\'' +
+                ", descripcion='" + descripcion + '\'' +
+                ", activo=" + activo +
+                ", usuarios=" + usuarios +
+                ", mensajes=" + mensajes +
+                '}';
+    }
 }
