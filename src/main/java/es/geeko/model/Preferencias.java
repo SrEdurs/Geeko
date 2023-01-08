881041f79ebb29eca5e25dda5b5b8d57a9192b8e
Aonpei <aonpei@gmail.com>
2023-01-08 17:50:51 +0100

    Tabla Preferencias que tiene Usuarios y Tematica.

---
+++ src/main/java/es/geeko/model/Preferencias.java
@@ -0,0 +1,60 @@
+package es.geeko.model;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "preferencias")
+public class Preferencias {
+
+    @Id
+    @Column(name = "idUsuario")
+    private int idUsuario;
+
+    @Id
+    @Column(name = "idTematica")
+    private int idTematica;
+
+    @ManyToOne
+    @JoinColumn(name = "id")
+    Usuario usuario;
+
+    @ManyToOne
+    @JoinColumn(name = "id")
+    Tematica tematica;
+
+
+    public Preferencias(int idUsuario, int idTematica) {
+        this.idUsuario = idUsuario;
+        this.idTematica = idTematica;
+    }
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+    public void setIdUsuario(int id) {
+        this.idUsuario = id;
+    }
+
+    public int getIdTematica() {
+        return idTematica;
+    }
+    public void setIdTematica(int id) {
+        this.idTematica = id;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+    public Tematica getTematica() {
+        return tematica;
+    }
+    public void setTematica(Tematica tematica) {
+        this.tematica = tematica;
+    }
+}
