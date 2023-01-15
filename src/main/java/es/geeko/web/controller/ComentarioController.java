fc48c32988329dd543379af7270f22581d4a01c5
Eduardo Rojas <edu_mairenero@hotmail.com>
2023-01-15 22:42:10 +0100

    - Añadidos controllers de Usuario, Comentario y Producto

---
+++ src/main/java/es/geeko/web/controller/ComentarioController.java
@@ -0,0 +1,46 @@
+package es.geeko.web.controller;
+
+import es.geeko.model.Comentario;
+import es.geeko.model.Usuario;
+import es.geeko.repository.ComentarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@CrossOrigin(origins = "http://localhost:8081")
+@RestController
+@RequestMapping("/api/comentario")
+public class ComentarioController {
+    @Autowired
+    ComentarioRepository comentarioRepository;
+
+    @GetMapping("/listacomentarios")
+    public ResponseEntity<List<Comentario>> getAllComentarios(){
+        try{
+            //Generamos el contenedor
+            List<Comentario> comentarios = new ArrayList<>();
+
+            //Leemos dentro del array el contenido del modelo virtual desde el repositorio
+            comentarioRepository.findAll().forEach(comentarios::add);
+
+            //Gestionar las respuestas
+            if(comentarios.isEmpty()){
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+
+            //Hay registros
+            return new ResponseEntity<>(comentarios, HttpStatus.OK);
+
+        }
+        catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
---
+++ src/main/java/es/geeko/web/controller/ProductoController.java
@@ -0,0 +1,46 @@
+package es.geeko.web.controller;
+
+import es.geeko.model.Producto;
+import es.geeko.repository.ProductoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@CrossOrigin(origins = "http://localhost:8081")
+@RestController
+@RequestMapping("/api/producto")
+public class ProductoController {
+    @Autowired
+    ProductoRepository productoRepository;
+
+
+    @GetMapping("/listaproductos")
+    public ResponseEntity<List<Producto>> getAllProductos(){
+        try{
+            //Generamos el contenedor
+            List<Producto> productos = new ArrayList<>();
+
+            //Leemos dentro del array el contenido del modelo virtual desde el repositorio
+            productoRepository.findAll().forEach(productos::add);
+
+            //Gestionar las respuestas
+            if(productos.isEmpty()){
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+
+            //Hay registros
+            return new ResponseEntity<>(productos, HttpStatus.OK);
+
+        }
+        catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
