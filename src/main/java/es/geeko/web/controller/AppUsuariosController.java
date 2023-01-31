8773d0453cbd412103ca064328945a9dde6fe6fa
Eduardo Rojas <edu_mairenero@hotmail.com>
2023-01-31 14:44:13 +0100

    - Inicio cuestionario con th:block

--- src/main/java/es/geeko/web/controller/AppUsuariosController.java
+++ src/main/java/es/geeko/web/controller/AppUsuariosController.java
@@ -1,21 +1,29 @@
 package es.geeko.web.controller;
 
 import es.geeko.dto.LoginDto;
+import es.geeko.dto.TematicaDto;
 import es.geeko.dto.UsuarioDto;
+import es.geeko.model.Tematica;
 import es.geeko.model.Usuario;
+import es.geeko.service.TematicaService;
 import es.geeko.service.UsuarioService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 import java.util.Optional;
 
 @Controller
 public class AppUsuariosController extends AbstractController<UsuarioDto> {
 
-    private final  UsuarioService service;
+    private final  UsuarioService usuarioService;
+    private final TematicaService tematicaService;
 
-    public AppUsuariosController(UsuarioService service) {
-        this.service = service;
+    public AppUsuariosController(UsuarioService usuarioService, TematicaService tematicaService) {
+        this.usuarioService = usuarioService;
+        this.tematicaService = tematicaService;
     }
 
     @GetMapping("")
@@ -35,11 +43,29 @@ public class AppUsuariosController extends AbstractController<UsuarioDto> {
     @PostMapping("/usuarios/crearcuenta")
     public String guardarUsuario(UsuarioDto usuarioDto) throws Exception {
         //LLamo al método del servicio para guardar los datos
-        UsuarioDto usuarioGuardado =  this.service.guardar(usuarioDto);
+        UsuarioDto usuarioGuardado =  this.usuarioService.guardar(usuarioDto);
         System.out.println(usuarioGuardado.getClave());
         Long id = usuarioGuardado.getId();
         return "usuarios/cuestionario";
     }
+
+    @GetMapping("/usuarios/cuestionario")
+    public String vistaCuestionario(Model interfazConPantalla){
+        final List<Tematica> tematicas = tematicaService.buscarEntidades();
+        interfazConPantalla.addAttribute("listaTematicas",tematicas);
+        return "/usuarios/cuestionario";
+    }
+
+    @PostMapping("/usuarios/cuestionario")
+    public String guardarCuestionario(UsuarioDto usuarioDtoEntrada) throws Exception{
+
+        UsuarioDto usuarioGuardado = this.usuarioService.guardar(usuarioDtoEntrada);
+        Long id = usuarioGuardado.getId();
+        return "perfil";
+
+    }
+
+
 
     //Controlador de Login
     @GetMapping("/usuarios/iniciarsesion")
@@ -54,7 +80,7 @@ public class AppUsuariosController extends AbstractController<UsuarioDto> {
         String clave = loginDto.getClave();
         System.out.println("clave = " + clave);
         //¿es correcta la password?
-        if (service.getRepo().validarClave(emilio,clave) > 0)
+        if (usuarioService.getRepo().validarClave(emilio,clave) > 0)
         {
             return "index";
         }else {
