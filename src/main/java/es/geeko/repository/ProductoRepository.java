b2947014ab37bea3a57f236de53301667c07e316
Eduardo Rojas <edu_mairenero@hotmail.com>
2023-02-06 21:44:45 +0100

    - Pantallas de productos con thymeleaf - Productos filtrados con findBy en ProductoRepository

--- src/main/java/es/geeko/repository/ProductoRepository.java
+++ src/main/java/es/geeko/repository/ProductoRepository.java
@@ -7,4 +7,20 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ProductoRepository extends JpaRepository<Producto,Integer> {
+
+    //Libro
+    List<Producto> findProductosByLibroAndActivo(int libro, int activo);
+    List<Producto> findProductosByLibroAndUsuarioIsNull(int libro);
+
+    //Pelicula
+    List<Producto> findProductosByPeliculaAndActivo(int pelicula, int activo);
+    List<Producto> findProductosByPeliculaAndUsuarioIsNull(int pelicula);
+
+    //Serie
+    List<Producto> findProductosBySerieAndActivo(int serie, int activo);
+    List<Producto> findProductosBySerieAndUsuarioIsNull(int serie);
+
+    //Videojuego
+    List<Producto> findProductosByVideojuegoAndActivo(int videojuego, int activo);
+    List<Producto> findProductosByVideojuegoAndUsuarioIsNull(int videojuego);
 }
--- src/main/java/es/geeko/web/controller/AppProductosController.java
+++ src/main/java/es/geeko/web/controller/AppProductosController.java
@@ -1,8 +1,13 @@
 package es.geeko.web.controller;
 
 import es.geeko.dto.ProductoDto;
+import es.geeko.dto.UsuarioDto;
+import es.geeko.model.Producto;
 import es.geeko.repository.ProductoRepository;
 import es.geeko.service.ProductoService;
+import es.geeko.service.UsuarioService;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,35 +21,78 @@ import java.util.Optional;
 public class AppProductosController extends AbstractController<ProductoDto> {
 
     private final ProductoService productoService;
+    private final ProductoRepository productoRepository;
+    private final UsuarioService usuarioService;
 
 
-    public AppProductosController(ProductoService service) {
+    public AppProductosController(ProductoService service, ProductoRepository productoRepository, UsuarioService usuarioService) {
         this.productoService = service;
-
+        this.productoRepository = productoRepository;
+        this.usuarioService = usuarioService;
     }
 
 
     @GetMapping("/productos/libros")
     public String vistaLibro(ModelMap interfazConPantalla){
-        final List<ProductoDto> listaProductos = productoService.buscarTodos();
+        final List<Producto> listaProductos = productoRepository.findProductosByLibroAndActivo(1,1);
         interfazConPantalla.addAttribute("listaProductos",listaProductos);
+        final List<Producto> listaNovedades = productoRepository.findProductosByLibroAndUsuarioIsNull(1);
+        interfazConPantalla.addAttribute("listaNovedades",listaNovedades);
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String username = authentication.getName();
+        Optional<UsuarioDto> usuarioDto = this.usuarioService.encuentraPorId(this.usuarioService.getRepo().findUsuarioByEmilio(username).get().getId());
+
+        UsuarioDto attr = usuarioDto.get();
+        interfazConPantalla.addAttribute("datosUsuario",attr);
         return "/productos/libros";
     }
 
     @GetMapping("/productos/peliculas")
     public String vistaPelis(ModelMap interfazConPantalla){
-        final List<ProductoDto> listaProductos = productoService.buscarTodos();
+        final List<Producto> listaProductos = productoRepository.findProductosByPeliculaAndActivo(1,1);
         interfazConPantalla.addAttribute("listaProductos",listaProductos);
+        final List<Producto> listaNovedades = productoRepository.findProductosByPeliculaAndUsuarioIsNull(1);
+        interfazConPantalla.addAttribute("listaNovedades",listaNovedades);
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String username = authentication.getName();
+        Optional<UsuarioDto> usuarioDto = this.usuarioService.encuentraPorId(this.usuarioService.getRepo().findUsuarioByEmilio(username).get().getId());
+
+        UsuarioDto attr = usuarioDto.get();
+        interfazConPantalla.addAttribute("datosUsuario",attr);
         return "/productos/peliculas";
     }
 
     @GetMapping("/productos/series")
-    public String vistaSerie(){
+    public String vistaSerie(ModelMap interfazConPantalla){
+        final List<Producto> listaProductos = productoRepository.findProductosBySerieAndActivo(1,1);
+        interfazConPantalla.addAttribute("listaProductos",listaProductos);
+        final List<Producto> listaNovedades = productoRepository.findProductosBySerieAndUsuarioIsNull(1);
+        interfazConPantalla.addAttribute("listaNovedades",listaNovedades);
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String username = authentication.getName();
+        Optional<UsuarioDto> usuarioDto = this.usuarioService.encuentraPorId(this.usuarioService.getRepo().findUsuarioByEmilio(username).get().getId());
+
+        UsuarioDto attr = usuarioDto.get();
+        interfazConPantalla.addAttribute("datosUsuario",attr);
         return "/productos/series";
     }
 
     @GetMapping("/productos/videojuegos")
-    public String vistaVideojuegos(){
+    public String vistaVideojuegos(ModelMap interfazConPantalla){
+        final List<Producto> listaProductos = productoRepository.findProductosByVideojuegoAndActivo(1,1);
+        interfazConPantalla.addAttribute("listaProductos",listaProductos);
+        final List<Producto> listaNovedades = productoRepository.findProductosByVideojuegoAndUsuarioIsNull(1);
+        interfazConPantalla.addAttribute("listaNovedades",listaNovedades);
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String username = authentication.getName();
+        Optional<UsuarioDto> usuarioDto = this.usuarioService.encuentraPorId(this.usuarioService.getRepo().findUsuarioByEmilio(username).get().getId());
+
+        UsuarioDto attr = usuarioDto.get();
+        interfazConPantalla.addAttribute("datosUsuario",attr);
         return "/productos/videojuegos";
     }
 
