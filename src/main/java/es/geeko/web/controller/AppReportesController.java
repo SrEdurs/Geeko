75aead59fd17eb59e3bb08a4c41193c588708eed
Eduardo Rojas <edu_mairenero@hotmail.com>
2023-02-21 13:48:05 +0100

    - Bug reportes solucionado - Enlace a cada elemento en los reportes

--- src/main/java/es/geeko/web/controller/AppReportesController.java
+++ src/main/java/es/geeko/web/controller/AppReportesController.java
@@ -260,7 +260,6 @@ public class AppReportesController extends AbstractController<ComentarioDto> {
         Optional<Usuario> usu = usuarioService.encuentraPorIdEntity(id);
         Optional<Producto> pro = productoService.encuentraPorIdEntity(id);
 
-        System.out.println("HOLA");
         //Si es un comentario
         if(coment.isPresent()){
             coment.get().setReportado(0);
