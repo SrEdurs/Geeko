b819b6600362dda60efe20e9437597d661e89283
Eduardo Rojas <edu_mairenero@hotmail.com>
2023-03-15 19:03:24 +0100

    - Bug de no megusta solucionado

--- src/main/java/es/geeko/GeekoApp.java
+++ src/main/java/es/geeko/GeekoApp.java
@@ -2,9 +2,16 @@ package es.geeko;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-public class GeekoApp {
+public class GeekoApp extends SpringBootServletInitializer {
+
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(GeekoApp.class);
+    }
 	public static void main(String[] args) {
 		SpringApplication.run(GeekoApp.class, args);
 	}
